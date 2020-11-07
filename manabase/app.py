4b418355314d7e9a1f673d96fdc538a2f7396819
Aphosis <aphosis.github@gmail.com>
2020-11-07 19:17:28 +0100

    📝 Added more TODOs

--- manabase/app.py
+++ manabase/app.py
@@ -36,6 +36,13 @@ def generate(
 
     client = Client()
 
+    # TODO: Cache the query results, it takes forever.
+    # TODO: Split data fetching from scryfall and filtering, `Client.fetch`
+    # should not do both. `Client` should not know about filters.
+    # TODO: Profile filtering. It currently tales forever, probably due to
+    # un-compiled regexes being used a pretty high number of times.
+    # TODO: Cache filtering results. It should be invalidated if the query
+    # cache is invalidated.
     cards = client.fetch(filters)
 
     print("\n".join([card.name for card in sorted(cards)]))
