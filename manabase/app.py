1cb90f02a98870825ec83a047be4a5dc515da36c
Aphosis <aphosis.github@gmail.com>
2020-11-08 15:02:11 +0100

    📝 Updated TODOs with issue numbers

--- manabase/app.py
+++ manabase/app.py
@@ -43,12 +43,10 @@ def generate(
 
     client = Client()
 
-    # TODO: Cache the query results, it takes forever.
-    # TODO: Split data fetching from scryfall and filtering, `Client.fetch`
+    # TODO: #2 Cache the query results, it takes forever.
+    # TODO: #3 Split data fetching from scryfall and filtering, `Client.fetch`
     # should not do both. `Client` should not know about filters.
-    # TODO: Profile filtering. It currently tales forever, probably due to
-    # un-compiled regexes being used a pretty high number of times.
-    # TODO: Cache filtering results. It should be invalidated if the query
+    # TODO: #1 Cache filtering results. It should be invalidated if the query
     # cache is invalidated.
     cards = client.fetch(filters)
 
--- manabase/filters/lands/base.py
+++ manabase/filters/lands/base.py
@@ -15,7 +15,7 @@ class LandFilter(CompositeFilter):
         self._regex = re.compile(self._process_pattern(pattern))
 
     def filter_value(self, value: Card) -> bool:
-        # TODO: Filter by extension first ?
+        # TODO: #4 Filter lands by extension first ?
         return bool(self._regex.match(value.oracle_text))
 
     @staticmethod
