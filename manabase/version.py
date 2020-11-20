73f896d38c2c338e4fa34352579df898a039eb66
Aphosis <aphosis.github@gmail.com>
2020-11-20 19:03:13 +0100

    ✅ Test __version__ matches pyproject.toml

--- manabase/version.py
+++ manabase/version.py
@@ -2,4 +2,4 @@
 
 Version is kept in its own file to avoid cyclic imports.
 """
-__version__ = "0.1.0"
+__version__ = "0.2.0"
--- tests/test_manabase.py
+++ tests/test_manabase.py
@@ -1,6 +1,15 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring
+import os
+
+import toml
+
 from manabase import __version__
 
 
 def test_version():
-    assert __version__ == "0.1.0"
+    root = os.path.dirname(os.path.dirname(__file__))
+    path = os.path.join(root, "pyproject.toml")
+
+    package = toml.load(path)
+
+    assert __version__ == package["tool"]["poetry"]["version"]
